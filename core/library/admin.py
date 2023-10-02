ad7ead23d46be1dd2c5844a2517cae64b1fd52b0
Veselov Misha <veselovmikhailwork@gmail.com>
2023-10-02 17:55:54 +0300

    setup admin

--- core/library/admin.py
+++ core/library/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from .models import Author, Book
+
+from .models import Author, Genre, Book
 
 
-admin.site.register(Book)
-admin.site.register(Author)
+class BookInline(admin.TabularInline):
+    model = Book
+    fields = ['title', 'pub_year', 'short_des']
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_day')
+    fields = [('name', 'birth_day'), 'short_bio']
+    inlines = [BookInline]
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'pub_year')
--- core/library/models.py
+++ core/library/models.py
@@ -4,19 +4,49 @@ from django.db import models
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=200)
-    birth_day = models.DateField()
-    short_bio = models.CharField(max_length=300, verbose_name='short biography')
+    """Model representing a author."""
+    name = models.CharField(max_length=200, help_text='name')
+    birth_day = models.DateField(blank=True, help_text='1900-01-01')
+    short_bio = models.CharField(
+        max_length=300, blank=True,  
+        help_text='biography', verbose_name='short biography'
+        )
 
     def __str__(self) -> str:
         return self.name
 
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(max_length=200, help_text = 'book genre')
+    
+    def __str__(self) -> str:
+        return self.name
+
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    pub_year = models.IntegerField(default=0, verbose_name='year of published')
-    short_des = models.CharField(max_length=300, verbose_name='short description')
-    image = models.ImageField(verbose_name='cover image')
+    """Model representing a book."""
+    title = models.CharField(max_length=200, help_text='title')
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, 
+        help_text='author'
+        )
+    pub_year = models.IntegerField(
+        default=0, blank=True, help_text='1900', 
+        verbose_name='year of published'
+        )
+    short_des = models.CharField(
+        max_length=300, blank=True, 
+        help_text='description', verbose_name='short description'
+        )
+    genre = models.ManyToManyField(Genre, help_text = 'genre')
+    image = models.ImageField(
+        help_text='Cover image', blank=True, 
+        verbose_name='cover image'
+        )
 
     def __str__(self) -> str:
         return self.title
+    
+    def display_genre(self) -> str:
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = 'Genre'
+    
