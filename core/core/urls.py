79460c4ad9df6379d9dd3462586f2c3060cd8bfc
Veselov Misha <veselovmikhailwork@gmail.com>
2023-10-05 12:20:30 +0300

    User authentication and permissions. Media/images

--- core/core/urls.py
+++ core/core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     re_path('^', include('library.urls')),
 ]
--- core/library/views.py
+++ core/library/views.py
@@ -1,35 +1,10 @@
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Book, Author, Genre
-
-
-def index(request):
-    """View function for home page of library site"""
-    num_books = Book.objects.count()
-    num_authors = Author.objects.count()
-    for genre in Genre.objects.all():
-        pass
-    context = {
-        'num_books': num_books,
-        'num_authors': num_authors,
-    }
-
-    return render(request, 'library/index.html', context=context)
-
-def addbook(request):
-    # если запрос POST, сохраняем данные
-    if request.method == "POST":
-        book = Book()
-        book.title = request.POST.get("title")
-        book.author_id = request.POST.get("author")
-        book.pub_year = request.POST.get("pub_year")
-        book.save()
-        return HttpResponseRedirect("/")
-    # передаем данные в шаблон
-    authors = Author.objects.all()
-    return render(request, "library/addbook.html", {"authors": authors})
+from .models import Book, Author
 
 
 class BookListView(generic.ListView):
@@ -37,7 +12,7 @@ class BookListView(generic.ListView):
     model = Book
     paginate_by = 10
 
-class BookDetailView(generic.DetailView):
+class BookDetailView(LoginRequiredMixin, generic.DetailView):
     """Представление детальной информации об авторе"""
     model = Book
 
@@ -50,3 +25,36 @@ class AuthorListView(generic.ListView):
 class AuthorDetailView(generic.DetailView):
     """Представление детальной информации о книге"""
     model = Author
+
+
+def index(request):
+    """View function for home page of library site"""
+    num_books = Book.objects.count()
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    return render(request, 'library/index.html', context=context)
+
+
+@login_required
+@permission_required('catalog.can_mark_returned')
+@permission_required('catalog.can_edit')
+def addbook(request):
+    # если запрос POST, сохраняем данные
+    if request.method == "POST":
+        book = Book()
+        book.title = request.POST.get("title")
+        book.author_id = request.POST.get("author")
+        book.pub_year = request.POST.get("pub_year")
+        book.save()
+        return HttpResponseRedirect("/")
+    # передаем данные в шаблон
+    authors = Author.objects.all()
+    return render(request, "library/addbook.html", {"authors": authors})
